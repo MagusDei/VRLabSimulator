c19fa2d6d85d337ec82579769a6ad1d41dbfd04e
hetasaara <hiltunen.saara@gmail.com>
2017-11-25 00:05:06 +0200

    scripts

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Click : MonoBehaviour {
+
+    public Text clickText;
+    public Text overText;
+    public Slider ragemeter;
+    public Button but1;
+
+    void Start()
+    {
+        clickText.text = "";
+        overText.text = "";
+        but1.gameObject.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        clickText.text = "Don't do it!";
+        ragemeter.value = ragemeter.value + 1;
+        
+    }
+    void OnMouseUpAsButton()
+    {
+        clickText.text = "";
+        gameOver();
+    }
+
+    void gameOver()
+    {
+        if (ragemeter.value >= 10)
+        {
+            overText.text = "Game over!";
+        }
+
+    }
+  
+}

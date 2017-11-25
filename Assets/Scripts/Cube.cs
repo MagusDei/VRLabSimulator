5cbf2ab91e3b3076aac4cdd34dd5595ab5354458
Snehadri <snehadri.sinha@gmail.com>
2017-11-25 21:54:39 +0200

    added 2 nodes and their prefabs

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+    public GameObject destroyed;
+
+    void OnMouseDown()
+    {
+        Instantiate(destroyed, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}

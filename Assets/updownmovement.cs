8fa3d1f5a859300e2f2ffccde7e516ebe095a39b
MagusDei <jami.valorinta@helsinki.fi>
2017-11-26 09:43:57 +0200

    Moving mugs

---
+++ Assets/updownmovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updownmovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x, transform.position.y + 0.002f*Mathf.Sin ((float)Time.time), transform.position.z);
+	}
+}
